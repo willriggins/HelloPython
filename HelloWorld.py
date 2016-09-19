2a2eaa1250cac7b794b5d31971e4db2f44af2947
willriggins <williamriggins2013@gmail.com>
2016-09-19 19:24:40 -0400

    printing with python

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,47 @@
 print ("My First Python Code")
 
-print ("More code!")
+#print ("commented out")
+
+
+'''
+commented out
+'''
+
+#assignment
+
+a = 0
+b = 3
+print(a + b)
+
+#assignmennt, strings
+c = "0"
+d = "3"
+print(c + d)
+
+#casting int as c
+print(int(c) + b)
+
+#conditionals
+
+if a >= 1:
+    print("if statement completes")
+elif a == -1:
+    print("elif statement completes")
+else:
+    print("else prints")
+
+#function
+def sayHello():
+    print("Hello!")
+
+sayHello()
+
+def addition(a, b):
+    print(a + b)
+
+addition(5, 15)
+
+#for loop
+
+for a in range(1,14): #2nd number is not inclusive
+    print(a)
